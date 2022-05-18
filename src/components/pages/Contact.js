3c064dd701419ecc6b5f39a19e302447f81319a2
Spencer Kerans <skerans0@gmail.com>
2022-05-18 12:27:51 -0600

    updated contact me page/resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,35 +1,88 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import '../../styles/Contact.css'
 
+function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
+    switch (name) {
+      case "username":
+        return setName(value);
+      case "email":
+        return setEmail(value);
+      case "message":
+        return setMessage(value);
+      default:
+        return;
+    }
+  };
 
-export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const [name, setName] = useState('')
-
-
-
-
-
-
+    setName("");
+    setEmail("");
+    setMessage("");
+    console.log("form submitted");
+  };
 
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div 
+    className="container">
+      <form>
+        <h2>Contact Me</h2>
+        <label>
+          Name:
+          <br/>
+          <input
+            className="contact-form"
+            type="text"
+            value={name}
+            name="username"
+            onChange={handleInputChange}
+            placeholder="name"
+          />
+        </label>
+        <br/>
+        <label>
+          Email:
+          <br/>
+          <input
+            className="contact-form"
+            type="email"
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            placeholder="email address"
+          />
+        </label>
+        <br/>
+        <label>
+          Message:
+          <br/>
+          <textarea
+            className="contact-form"
+            name="message"
+            value={message}
+            onChange={handleInputChange}
+            placeholder="message"
+          />
+        </label>
+        <br/>
+        <button 
+        type="button" 
+        className="btn btn-primary"
+        onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Contact;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,14 +4,21 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <p>Check out my <span>Resume</span></p>
+      <h3>Coding Experience</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>JQuery</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Express</li>
+      </ul>
+      
+
     </div>
   );
 }
